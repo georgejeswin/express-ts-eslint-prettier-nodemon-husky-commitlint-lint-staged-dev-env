26123f7a82424b8db8bf5e4da60f291091f12050
georgejeswin <jeswinmyladoor@gmail.com>
2023-04-16 02:52:34 +0530

    Added express and start script

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send();
+});
+
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
