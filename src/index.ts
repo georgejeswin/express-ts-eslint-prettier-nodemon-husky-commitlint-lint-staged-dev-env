80e674684a9293da25bb99a17af2e1051d7fb316
georgejeswin <jeswinmyladoor@gmail.com>
2023-04-16 14:28:01 +0530

    updated eslint config, added husky and scripts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
-import express, { Request, Response } from "express";
-import dotenv from "dotenv";
+import express, { type Request, type Response } from 'express';
+import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ?? 5000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.status(200).send("Project Starter");
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).send('Project Starter');
 });
 
-app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`);
+});
